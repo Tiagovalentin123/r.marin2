df73c836e2869a000624a5280f4f082ae4a0fc89
Tiagovalentin123 <tiagovalentin3@gmail.com>
2025-05-28 14:50:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"message": "Olá, Render com FastAPI!"}
+def home():
+    return {"message": "Hello, Render!"}
+
+# Adicione esta parte se estiver usando variáveis de ambiente ou configurações extras
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=10000)
